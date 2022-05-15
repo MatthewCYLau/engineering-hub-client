f21dc5da3419f4c1ad74a80526474b01885cff25
matthew.lau <lau.cy.matthew@gmail.com>
2022-05-15 12:15:50 +0100

    feat: added registration screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,7 +37,7 @@
                   >Sign In</a
                 >
                 <a
-                  href="#_"
+                  href="/register"
                   class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-indigo-600 md:px-3 md:w-auto md:rounded-full lg:px-5 hover:bg-indigo-500 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-indigo-600"
                   >Sign Up</a
                 >
@@ -102,7 +102,7 @@
         <div class="flex flex-col items-center mt-12 text-center">
           <span class="relative inline-flex w-full md:w-auto">
             <a
-              href="/"
+              href="/register"
               class="inline-flex items-center justify-center w-full px-8 py-4 text-base font-bold leading-6 text-white bg-indigo-600 border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
             >
               Sign Up
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,45 +1,154 @@
 <template>
-  <form @submit.prevent="submit">
-    <h1 class="h3 mb-3 fw-normal">Please register</h1>
+  <div class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
+    <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+      <form @submit.prevent="submit">
+        <h1 class="font-bold text-center text-2xl mb-5">Engineering Hub</h1>
 
-    <input v-model="data.name" class="form-control" placeholder="Name" required>
+        <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+          <div class="px-5 py-7">
+            <label class="font-semibold text-sm text-gray-600 pb-1 block"
+              >E-mail</label
+            >
 
-    <input v-model="data.email" type="email" class="form-control" placeholder="Email" required>
+            <input
+              aria-label="Email address"
+              name="email"
+              type="email"
+              required
+              class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+              placeholder="Email address"
+              v-model="email"
+            />
+            <label class="font-semibold text-sm text-gray-600 pb-1 block"
+              >Password</label
+            >
+            <input
+              aria-label="Password"
+              name="password"
+              type="password"
+              required
+              class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+              placeholder="Password"
+              v-model="password"
+            />
+            <label class="font-semibold text-sm text-gray-600 pb-1 block"
+              >Joined Date</label
+            >
+            <div class="-mt-px mb-4">
+              <input
+                aria-label="Joined Date"
+                name="joinedDate"
+                type="date"
+                class="appearance-none rounded-lg  relative block w-full px-3 py-2 border placeholder-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                placeholder="Joined Date"
+                v-model="joinedDate"
+              />
+            </div>
 
-    <input v-model="data.password" type="password" class="form-control" placeholder="Password" required>
-
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
-  </form>
+            <button
+              type="submit"
+              class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+            >
+              <span class="inline-block mr-2">Sign Up</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                class="w-4 h-4 inline-block"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div class="py-5">
+          <div class="grid grid-cols-2 gap-1">
+            <div class="text-center sm:text-left whitespace-nowrap">
+              <button
+                class="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-200 focus:outline-none focus:bg-gray-300 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  class="w-4 h-4 inline-block align-text-top"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M10 19l-7-7m0 0l7-7m-7 7h18"
+                  />
+                </svg>
+                <router-link to="/"
+                  ><span class="inline-block ml-1"
+                    >Back to home</span
+                  ></router-link
+                >
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import {reactive} from 'vue';
-import {useRouter} from "vue-router";
+import { defineComponent, reactive, toRefs } from "vue";
+import { useRouter } from "vue-router";
+import { useApi } from "../modules/api";
 
-export default {
-  name: "Register",
+interface LoginPayload {
+  email?: string;
+  password?: string;
+  joinedDate?: Date;
+}
+
+const transformDateToDdMmYyyy = (joinedDate: Date): string => {
+  const dateParts = joinedDate.toString().split("-");
+  const dd = dateParts[2];
+  const mm = dateParts[1];
+  const yyyy = dateParts[0];
+  return dd + "/" + mm + "/" + yyyy;
+};
+
+export default defineComponent({
   setup() {
-    const data = reactive({
-      name: '',
-      email: '',
-      password: ''
-    });
+    const { loading, data, post } = useApi("api/users");
+
     const router = useRouter();
 
-    const submit = async () => {
-      await fetch('http://localhost:8000/api/register', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-      });
+    const payload = reactive<LoginPayload>({
+      email: undefined,
+      password: undefined,
+      joinedDate: undefined,
+    });
 
-      await router.push('/login');
-    }
+    const submit = () => {
+      const transformedPayload = {
+        ...payload,
+        joinedDate:
+          payload.joinedDate && transformDateToDdMmYyyy(payload.joinedDate),
+      };
+      post(transformedPayload).then(() => {
+        localStorage.setItem("token", data.value.token);
+        router.push({ name: "dashboard" });
+      });
+    };
 
     return {
-      data,
-      submit
-    }
-  }
-}
+      loading,
+      submit,
+      ...toRefs(payload),
+    };
+  },
+});
 </script>
