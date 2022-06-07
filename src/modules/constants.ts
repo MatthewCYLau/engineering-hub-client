c76a1e98ebb84949fcacaedad561bdb4b58948b7
matthew.lau <lau.cy.matthew@gmail.com>
2022-06-07 22:28:47 +0100

    feat: added pagination front-end logic

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -1,4 +1,4 @@
-const PAGE_SIZE = 2;
+const PAGE_SIZE = 6;
 
 export default {
   PAGE_SIZE: PAGE_SIZE,
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -46,20 +46,81 @@
           />
         </div>
       </div>
+      <div class="flex mt-4">
+        <button
+          class="border border-indigo-500 text-indigo-500 block rounded-sm font-bold py-4 px-6 mr-2 flex items-center"
+          @click="handleOnPreviousPageClick"
+          :disabled="shouldDisablePreviousButton"
+          :class="{
+            'bg-indigo-500 hover:bg-indigo-400 hover:text-white text-white ':
+              !shouldDisablePreviousButton,
+          }"
+        >
+          <svg
+            class="h-5 w-5 mr-2 fill-current"
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="-49 141 512 512"
+            style="enable-background: new -49 141 512 512"
+            xml:space="preserve"
+          >
+            <path
+              id="XMLID_10_"
+              d="M438,372H36.355l72.822-72.822c9.763-9.763,9.763-25.592,0-35.355c-9.763-9.764-25.593-9.762-35.355,0 l-115.5,115.5C-46.366,384.01-49,390.369-49,397s2.634,12.989,7.322,17.678l115.5,115.5c9.763,9.762,25.593,9.763,35.355,0 c9.763-9.763,9.763-25.592,0-35.355L36.355,422H438c13.808,0,25-11.193,25-25S451.808,372,438,372z"
+            ></path>
+          </svg>
+          Previous page
+        </button>
+        <button
+          class="border border-indigo-500 text-indigo-500 block rounded-sm font-bold py-4 px-6 ml-2 flex items-center"
+          @click="handleOnNextPageClick"
+          :disabled="shouldDisableNextButton"
+          :class="{
+            'bg-indigo-500 hover:bg-indigo-400 hover:text-white text-white':
+              !shouldDisableNextButton,
+          }"
+        >
+          Next page
+          <svg
+            class="h-5 w-5 ml-2 fill-current"
+            clasversion="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="-49 141 512 512"
+            style="enable-background: new -49 141 512 512"
+            xml:space="preserve"
+          >
+            <path
+              id="XMLID_11_"
+              d="M-24,422h401.645l-72.822,72.822c-9.763,9.763-9.763,25.592,0,35.355c9.763,9.764,25.593,9.762,35.355,0
+            l115.5-115.5C460.366,409.989,463,403.63,463,397s-2.634-12.989-7.322-17.678l-115.5-115.5c-9.763-9.762-25.593-9.763-35.355,0
+            c-9.763,9.763-9.763,25.592,0,35.355l72.822,72.822H-24c-13.808,0-25,11.193-25,25S-37.808,422-24,422z"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   </section>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, watch, reactive } from "vue";
+import { defineComponent, computed, watch, reactive, toRefs } from "vue";
 import { useApiWithAuth } from "../modules/api";
 import TaskCard from "../components/TaskCard.vue";
 import { useAuth, loadUser } from "../modules/auth";
 import constants from "../modules/constants";
 import { usePagination } from "../modules/pagination";
 
-interface Pagination {
+interface DashboardState {
   currentPage: number;
+  currentTasks: any[];
 }
 
 export default defineComponent({
@@ -68,27 +129,58 @@ export default defineComponent({
     loadUser();
     const { user } = useAuth();
     const { setItemsCount, itemsCount } = usePagination();
-    const paginationState = reactive<Pagination>({
+    const dashboardState = reactive<DashboardState>({
       currentPage: 1,
+      currentTasks: [],
     });
-    const { loading, data, get } = useApiWithAuth(
-      `/api/tasks?pageSize=${constants.PAGE_SIZE.toString()}&page=${
-        paginationState.currentPage
-      }`
+
+    const fetchData = (): void => {
+      const { get, data } = useApiWithAuth(
+        `/api/tasks?pageSize=${constants.PAGE_SIZE.toString()}&page=${
+          dashboardState.currentPage
+        }`
+      );
+      get().then((data) => (dashboardState.currentTasks = data.tasks));
+      watch([data], () => {
+        if (data.value) {
+          setItemsCount(data.value.tasksCount);
+        }
+      });
+    };
+    fetchData();
+    // const initials = computed(() => user?.value && user.value.email);
+
+    const tasks = computed(() => dashboardState.currentTasks);
+
+    const handleOnNextPageClick = () => {
+      dashboardState.currentPage++;
+      fetchData();
+    };
+
+    const handleOnPreviousPageClick = () => {
+      dashboardState.currentPage--;
+      fetchData();
+    };
+
+    const shouldDisablePreviousButton = computed(
+      () => dashboardState.currentPage === 1
     );
 
-    get();
-    const initials = computed(() => user?.value && user.value.email);
-    const tasks = computed(() => data.value && data.value.tasks);
+    const shouldDisableNextButton = computed(
+      () =>
+        Math.ceil(itemsCount.value / constants.PAGE_SIZE) ===
+        dashboardState.currentPage
+    );
 
-    watch([data], () => {
-      if (data.value) {
-        setItemsCount(data.value.tasksCount);
-        console.log(itemsCount.value);
-      }
-    });
-    console.log(itemsCount.value);
-    return { user, tasks, loading, initials };
+    return {
+      user,
+      handleOnNextPageClick,
+      handleOnPreviousPageClick,
+      tasks,
+      shouldDisablePreviousButton,
+      shouldDisableNextButton,
+      ...toRefs(dashboardState),
+    };
   },
   onMounted() {
     // loadUser();
